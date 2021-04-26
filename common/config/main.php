b9cf48e6632dacff0d4e659534faa13628c969a5
Alejandro Zanlongo <azanlongo@gmail.com>
2021-04-25 23:17:54 -0300

    config mailer in main

--- common/config/main.php
+++ common/config/main.php
@@ -37,5 +37,18 @@ return [
                 'database' => 0,
             ],
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'port' => '1025',
+                'host' => 'mail'
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+        ],
     ]
 ];
